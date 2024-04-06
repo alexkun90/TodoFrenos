4f1752a55e554e4693bb9bc8a2ad4b4ece9274bb
CharlieAcl <CharlieCalderon003@gmail.com>
2024-04-05 21:11:25 -0600

    Interfaz Citas/Vehiculo-Inicio

---
+++ ProyectoTodoFrenosWeb/ProyectoTodoFrenosWeb/Controllers/CitaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoTodoFrenosWeb.Controllers
+{
+    public class CitaController : Controller
+    {
+        [HttpGet]
+        public IActionResult SolicitudCita()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AdminCitas()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult EvaluarCitas()
+        {
+            return View();
+        }
+    }
+}
