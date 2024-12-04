720c43947811a41900583c395bd142b2b1e0607a
AdrianCampos21 <106779974+AdrianCampos21@users.noreply.github.com>
2024-12-03 18:52:26 -0600

    Correcion de mensajes

--- ProyectoTodoFrenosWeb/DAL/Models/Product.cs
+++ ProyectoTodoFrenosWeb/DAL/Models/Product.cs
@@ -14,16 +14,21 @@ public partial class Product
     [DisplayName("Categoría")]
     public long? CategoryId { get; set; }
     [DisplayName("Nombre")]
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
     public string? ProductName { get; set; }
 
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
     public int? Stock { get; set; }
-    
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
     [DisplayName("Precio")]
     public decimal? Price { get; set; }
-    
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
     [DisplayName("Estado")]
     public bool StateProdc { get; set; } = true;
-    
+
     [DisplayName("Imagen")]
     public byte[]? ImageProduct { get; set; }
     
